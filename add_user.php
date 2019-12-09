5d3bbd9503a19287edf65fdbf588c28291c1aa25
sha <unanimity@list.ru>
2019-12-09 17:42:01 -0500

    in process ...

--- add_user.php
+++ add_user.php
@@ -4,7 +4,7 @@
   // Checkin What level user has permission to view this page
   page_require_level(1);
 $groups = find_all('user_groups');
-$groups = find_all('organisations');
+$organisations = find_all('organisations');
 ?>
 <?php
   if(isset($_POST['add_user'])){
