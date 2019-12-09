230e47abf23d06b7f6901101e8cb1ad452483c22
sha <unanimity@list.ru>
2019-12-09 16:36:30 -0500

    add product info 3

--- includes/sql.php
+++ includes/sql.php
@@ -210,7 +210,14 @@ function tableExists($table){
    /*--------------------------------------------------------------*/
   function join_product_table(){
      global $db;
-     $sql  =" SELECT p.id,p.name,p.quantity,p.buy_price,p.sale_price,p.media_id,p.date,c.name";
+     $sql  =" SELECT p.id,p.name,p.quantity,p.buy_price,p.sale_price,p.media_id,p.date,
+    p.`discription`,
+    p.`value`,
+    p.`dimension`,
+    p.`type`,
+    p.`owner`,
+    p.`cell`,
+    p.`code`,c.name";
     $sql  .=" AS categorie,m.file_name AS image";
     $sql  .=" FROM products p";
     $sql  .=" LEFT JOIN categories c ON c.id = p.categorie_id";
