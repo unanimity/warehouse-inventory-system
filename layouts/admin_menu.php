4da13e9297481788705d7c1e29c78cf7341e3087
sha <unanimity@list.ru>
2019-12-09 18:01:20 -0500

    in process ...

--- layouts/admin_menu.php
+++ layouts/admin_menu.php
@@ -1,3 +1,11 @@
+<?php function debug_to_console($data) {
+$output = $data;
+if (is_array($output))
+$output = implode(',', $output);
+
+echo "<script>console.log('Debug Objects: " . $output . "' );</script>";
+}
+?>
 <ul>
   <li>
     <a href="admin.php">
