6ec21915822119f4b14461821996c73007ef16f2
sha <unanimity@list.ru>
2019-12-09 22:11:59 -0500

    in process ...

--- layouts/admin_menu.php
+++ layouts/admin_menu.php
@@ -48,15 +48,26 @@
          <li><a href="add_sale.php">Add Sale</a> </li>
      </ul>
   </li>
-  <li>
-    <a href="#" class="submenu-toggle">
-      <i class="glyphicon glyphicon-signal"></i>
-       <span>Sales Report</span>
-      </a>
-      <ul class="nav submenu">
-        <li><a href="sales_report.php">Sales by dates </a></li>
-        <li><a href="monthly_sales.php">Monthly sales</a></li>
-        <li><a href="daily_sales.php">Daily sales</a> </li>
-      </ul>
-  </li>
+    <li>
+        <a href="#" class="submenu-toggle">
+            <i class="glyphicon glyphicon-signal"></i>
+            <span>Sales Report</span>
+        </a>
+        <ul class="nav submenu">
+            <li><a href="sales_report.php">Sales by dates </a></li>
+            <li><a href="monthly_sales.php">Monthly sales</a></li>
+            <li><a href="daily_sales.php">Daily sales</a> </li>
+        </ul>
+    </li>
+    <li>
+        <a href="#" class="submenu-toggle">
+            <i class="glyphicon glyphicon-signal"></i>
+            <span>Storage</span>
+        </a>
+        <ul class="nav submenu">
+
+            <li><a href="storage.php">Monthly cells</a></li>
+
+        </ul>
+    </li>
 </ul>
