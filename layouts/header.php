1fd8c5a19fbd282da201034031ee4bedf503c0f3
sha <unanimity@list.ru>
2020-01-30 18:50:37 -0500

    p

--- layouts/header.php
+++ layouts/header.php
@@ -21,6 +21,9 @@
       <div class="header-date pull-left">
         <strong><?php echo date("F j, Y, g:i a");?></strong>
       </div>
+          <div class="header-right">
+              <img src="../libs/images/icons8-24.png">
+          </div>
       <div class="pull-right clearfix">
         <ul class="info-menu list-inline list-unstyled">
           <li class="profile">
